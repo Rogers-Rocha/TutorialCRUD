80f5534f21ef33713041f63ed6795cadf0fe11d1
Rogers Rocha <rogerscarvalho13@hotmail.com>
2024-09-26 11:51:16 -0300

    float cont em exibir aprovados + função buscar aluno
    So é possivel verificar uma media se uma das variaveis for float. Logo, escolheu-se pelo contador ser float.
    
    Implementado a função buscar aluno, inclusive mostrando todos os alunos de mesmo nome com suas respectivas informações

--- p1.c
+++ p1.c
@@ -30,6 +30,7 @@ struct Aluno{
 int menu(void), verificaLista(void), volt(), Cadastrar(int fim, int Cad);
 void inicia(void);
 void listar(void), exibirAprovados(void);
+void buscarAluno(void);
 
 int main(void){
     
@@ -47,6 +48,9 @@ int main(void){
                 break;
             case 3:
                 exibirAprovados();
+                break;
+            case 4:
+                buscarAluno();
                 break;
             case 8:
                 printf("\nSaindo do programa...\n");
@@ -175,20 +179,59 @@ void listar(void){
 }
 
 void exibirAprovados(void){
-    int cont = 0;
-    float aprov; 
+    system("cls");
+    float cont = 0, aprov; 
     printf("-------------------------------------------------------");
     printf("\nExibicao dos alunos aprovados:\n");
     for(int i = 0; i < ind; i ++){
-        if(Cadastro[i].Disc.media > 7){
+        if(Cadastro[i].Disc.media >= 7){
             cont++;
-            printf("\nCont: %d", cont);
             printf("\nAluno %d: %s aprovado com media %.2f", i + 1, Cadastro[i].aluno, Cadastro[i].Disc.media);
         }
     }
-    aprov = (cont/ind)*100;
-    printf("\n\n%.2f por cento da sala foi aprovada.\n", aprov);
+    if (ind > 0) {
+        aprov = (cont/ind)*100;
+        printf("\n\n%.f%% da sala foi aprovada.\n", aprov);
+    } else {
+        printf("\n\nNao ha alunos cadastrados para calcular a porcentagem.\n");
+    }
 }
+
+void buscarAluno(void){
+    system("cls");
+    int i = 0, pos, exit = 2;
+    char aluno2[40];
+
+    while (exit == 2) {
+        if (ind == 0) {
+            printf("\nNenhum aluno cadastrado.\n");
+            return;
+        }
+        printf("\nDigite o nome do aluno que deseja encontrar: ");
+        fgets(aluno2, 40, stdin);
+        aluno2[strcspn(aluno2, "\n")] = '\0'; 
+
+        int encontrado = 0; 
+        for (i = 0; i < ind; i++) {
+            if (strcmp(Cadastro[i].aluno, aluno2) == 0) {
+                encontrado = 1;
+                
+
+                printf("\nO aluno '%s' possui id %d no cadastro.", aluno2, i + 1);
+                printf("\nNotas: %.2f | %.2f | %.2f      Media: %.2f\n", Cadastro[i].Disc.notas[0], Cadastro[i].Disc.notas[1], Cadastro[i].Disc.notas[2], Cadastro[i].Disc.media);
+                
+            }
+        }
+
+        if (encontrado == 0) {
+            printf("\nO aluno '%s' nao foi encontrado.\n", aluno2);
+        }
+
+        exit = volt(); 
+    }
+}
+
+
 
 int volt()
 {
